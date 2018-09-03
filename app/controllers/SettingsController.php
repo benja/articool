dbf7c6478c379c30b13a9244b63509244cf19f69
Benjamin Akar <benjaminakar2001@gmail.com>
2018-09-03 21:01:43 +0200

    Fixed settingscontroller

--- app/controllers/SettingsController.php
+++ app/controllers/SettingsController.php
@@ -6,13 +6,13 @@ class SettingsController extends ControllerBase
     public function profileAction()
     {
         $this->restrictAccess('user'); // restrict access to user only
-    	$this->view->user = $this->_user; // pass user information to view
+        $this->view->user = $this->_user; // pass user information to view
     }
 
     public function securityAction()
     {
         $this->restrictAccess('user'); // restrict access to user only
-    	$this->view->user = $this->_user; // pass user information to view
+        $this->view->user = $this->_user; // pass user information to view
     }
 
 }
